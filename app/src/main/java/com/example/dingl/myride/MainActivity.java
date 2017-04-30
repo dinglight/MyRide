b9356504db65757a702c48761fb657702576e96f
dingliang <liang.ding@samsung.com>
2017-04-30 12:41:08 +0800

    add toggle button

---
+++ app/src/main/java/com/example/dingl/myride/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.dingl.myride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.togglebutton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                } else {
+
+                }
+            }
+        }
+        );
+    }
+}
